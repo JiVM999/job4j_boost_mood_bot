37a01b335f2d36f689bc38bd8d8e17a67cfbf4a7
JiVM999 <mikhail.zenkov.1994@mail.ru>
2024-11-25 00:50:23 +0300

    Жизненный цикл Spring Bean [#505292]

--- src/main/java/ru/job4j/bmb/RecommendationEngine.java
+++ src/main/java/ru/job4j/bmb/RecommendationEngine.java
@@ -1,4 +1,17 @@
 package ru.job4j.bmb;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class RecommendationEngine  {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean RecommendationEngine is going through init.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean RecommendationEngine will be destroyed now.");
+    }
 }
--- src/main/java/ru/job4j/bmb/ReminderService.java
+++ src/main/java/ru/job4j/bmb/ReminderService.java
@@ -1,4 +1,19 @@
 package ru.job4j.bmb;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.stereotype.Service;
+
+@Service
 public class ReminderService {
+
+    @PostConstruct
+    public void init() {
+        System.out.println("Bean TelegramBotService is going through init.");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Bean TelegramBotService will be destroyed now.");
+    }
 }
