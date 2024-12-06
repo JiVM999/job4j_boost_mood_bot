f0f803e303494af551a868511140677a7b7dd4fc
JiVM999 <mikhail.zenkov.1994@mail.ru>
2024-12-06 19:17:38 +0300

    Механизм рассылки уведомлений [#505297]

--- src/main/java/ru/job4j/bmb/services/TgRemoteService.java
+++ src/main/java/ru/job4j/bmb/services/TgRemoteService.java
@@ -9,6 +9,8 @@ import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import ru.job4j.bmb.model.User;
+import ru.job4j.bmb.repository.UserFakeRepository;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -21,11 +23,14 @@ public class TgRemoteService extends TelegramLongPollingBot {
     private final String botName;
     private final String botToken;
     private Map<String, String> moodResp = new HashMap<>();
+    private final UserFakeRepository userRepository;
 
     public TgRemoteService(@Value("${telegram.bot.name}") String botName,
-                           @Value("${telegram.bot.token}") String botToken) {
+                           @Value("${telegram.bot.token}") String botToken,
+                           UserFakeRepository userRepository) {
         this.botName = botName;
         this.botToken = botToken;
+        this.userRepository = userRepository;
         moodResp.put("lost_sock", "Носки — это коварные создания. Но не волнуйся, второй обязательно найдётся!");
         moodResp.put("cucumber", "Огурец тоже дело серьёзное! Главное, не мариноваться слишком долго.");
         moodResp.put("dance_ready", "Супер! Танцуй, как будто никто не смотрит. Или, наоборот, как будто все смотрят!");
@@ -43,7 +48,7 @@ public class TgRemoteService extends TelegramLongPollingBot {
         return botName;
     }
 
-    private void send(SendMessage message) {
+    public void send(SendMessage message) {
         try {
             execute(message);
         } catch (TelegramApiException e) {
@@ -80,14 +85,16 @@ public class TgRemoteService extends TelegramLongPollingBot {
 
     @Override
     public void onUpdateReceived(Update update) {
-        if (update.hasCallbackQuery()) {
-            var data = update.getCallbackQuery().getData();
-            var chatId = update.getCallbackQuery().getMessage().getChatId();
-            send(new SendMessage(String.valueOf(chatId), moodResp.get(data)));
-        }
         if (update.hasMessage() && update.getMessage().hasText()) {
-            long chatId = update.getMessage().getChatId();
-            send(sendButtons(chatId));
+            var message = update.getMessage();
+            if ("/start".equals(message.getText())) {
+                long chatId = message.getChatId();
+                var user = new User();
+                user.setClientId(message.getFrom().getId());
+                user.setChatId(chatId);
+                userRepository.save(user);
+                send(sendButtons(chatId));  // Метод для отправки кнопок пользователю
+            }
         }
     }
 
